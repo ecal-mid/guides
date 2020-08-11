c0b9383a798a6e70056869a6bb84355e1aa4328a
Sébastien Matos <sess22@gmail.com>
2020-08-11 16:51:24 +0200

    Add code convertable

--- troubleshoot/js/main.js
+++ troubleshoot/js/main.js
@@ -1,6 +1,23 @@
-window.addEventListener('load', init)
+window.addEventListener("load", init);
 
-function init() {
+async function init() {
+  const options = {
+    headers: new Headers({
+      "content-type": "text/html",
+    }),
+  };
+    // "https://raw.githubusercontent.com/ecal-mid/guides/master/README.md";
+  const url =`./README.md?dev=${Date.now()}`;
+  const resp = await fetch(url, options);
+  const checklistMarkdown = await resp.text();
+  console.log(url, checklistMarkdown)
+  document.body.innerHTML = marked(checklistMarkdown);
 
+  document.querySelectorAll('ul input[type="checkbox"]').forEach(remapCheckBox);
+}
 
+function remapCheckBox(checkbox, index) {
+  checkbox.disabled = false;
+  checkbox.name = `checkbox-${index}`;
+  console.log(checkbox.nextSibling);
 }
