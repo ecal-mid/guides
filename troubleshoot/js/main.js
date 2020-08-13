02cf6cb83f5306c9d6629b04ad2b67a736ac4663
Sébastien Matos <sess22@gmail.com>
2020-08-13 18:48:35 +0200

    Fix hyperlink add set new tab

--- troubleshoot/js/main.js
+++ troubleshoot/js/main.js
@@ -7,7 +7,7 @@ async function init() {
     }),
   };
   // "https://raw.githubusercontent.com/ecal-mid/guides/master/README.md";
-  const url = noCacheURL('./README.md');
+  const url = noCacheURL("./README.md");
   const resp = await fetch(url, options);
   const checklistMarkdown = await resp.text();
 
@@ -19,6 +19,8 @@ async function init() {
     .forEach(remapCheckBox);
   document.body.appendChild(mainContainer);
 
+  document.querySelectorAll("a").forEach(fixHyperlink);
+
   document
     .querySelectorAll("h1, h2")
     .forEach((elem) =>
@@ -27,6 +29,18 @@ async function init() {
         "javascript:window.location.href=window.location.href"
       )
     );
+}
+
+function fixHyperlink(hyperLinkTag) {
+  let regExp = /\(([^)]+)\)/;
+  let href = regExp.exec(hyperLinkTag.href)[1];
+  hyperLinkTag.href = href || hyperLinkTag.href;
+  //matches[1] contains the value between the parentheses
+
+  hyperLinkTag.target = "_blank";
+  hyperLinkTag.rel = "external";
+  console.log(hyperLinkTag);
+  return hyperLinkTag;
 }
 
 function noCacheURL(url) {
@@ -54,8 +68,6 @@ function remapCheckBox(checkbox, index) {
   prt.appendChild(label);
 
   setCollapse(container);
-
-  // console.log(prt.childNodes)
 }
 
 function clickableHeading(elem, link) {
@@ -67,23 +79,10 @@ function clickableHeading(elem, link) {
   return aTag;
 }
 
-function clickCollapse(e) {
-  e.preventDefault();
-  this.previousSibling.classList.toggle("collapsed");
-}
-
 function setCollapse(container) {
   container.querySelectorAll(":scope>ul").forEach(function (ul) {
-    // let item = ul.parentElement;
-    // let btn = cloneTemplate('.btn-more');
-    // item.appendChild(btn)
-    // item.classList.toggle('collapsed');
-
     let siblings = getPreviousSiblings(ul);
-    // summaryInner.parentElement.insertBefore(details, ul);
-    // let clickable = document.createElement('div');
-    // prev.parentElement.insertBefore('clickable', prev)
-    console.log(siblings);
+
     let summary = document.createElement("summary");
     let details = document.createElement("details");
     ul.parentElement.insertBefore(details, ul);
@@ -95,11 +94,6 @@ function setCollapse(container) {
     prt.parentElement.insertBefore(details, prt);
     prt.remove();
   });
-
-  function toggleCollapse() {
-    console.log("hey", this);
-    // this.classList.toggle('collapsed')
-  }
 }
 
 function cloneTemplate(selector, deep = false) {
